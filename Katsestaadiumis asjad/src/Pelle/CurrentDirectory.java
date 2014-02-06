932d45befabcfd2a5d3c4866e2588e58aa71f789
pele <pelleproosa@gmail.com>
2014-02-07 01:50:16 +0200

    Current Directory näide kliendi kaartide kataloogi jaoks

--- Katsestaadiumis asjad/src/Pelle/CurrentDirectory.java
+++ Katsestaadiumis asjad/src/Pelle/CurrentDirectory.java
@@ -8,10 +8,10 @@ public class CurrentDirectory {
 	public static void start() {
 		// TODO Auto-generated method stub
 
-		/*
+		
 		System.out.println("Working Directory = " +
 				System.getProperty("user.dir"));
-		*/
+		
 		
 	//	Path currentRelativePath = Paths.get("");
 	//	String s = currentRelativePath.toAbsolutePath().toString();
--- Katsestaadiumis asjad/src/Pelle/PelleMain.java
+++ Katsestaadiumis asjad/src/Pelle/PelleMain.java
@@ -5,6 +5,7 @@ public class PelleMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Pelle programm jookseb!");
+		
 		Mysql.start();
 		CurrentDirectory.start();
 	}
